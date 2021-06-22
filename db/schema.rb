a971dbf82380484b18ee1be91de7852289c6bb82
naonao-program <gf19a012@oecu.jp>
2021-06-22 06:07:03 +0000

    指摘があったところを修正2

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2021_06_18_133425) do
   end
 
   create_table "books", force: :cascade do |t|
-    t.text "title"
+    t.string "title"
     t.text "body"
     t.integer "user_id"
     t.datetime "created_at", null: false
